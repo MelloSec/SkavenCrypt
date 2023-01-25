ab638ec2c29978d31f03f8fdf819b2e22a4a53de
mellonaut <65114647+mellonaut@users.noreply.github.com>
2023-01-25 14:01:52 -0500

    First test, not working

---
+++ SkavenCrypt.cs
@@ -0,0 +1,65 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using System.Text;
+
+class SkavenCrypt
+{
+    static void Main(string[] args)
+    {
+        // Check if the correct number of arguments have been provided
+        if (args.Length < 3)
+        {
+            Console.WriteLine("Invalid number of arguments.");
+            return;
+        }
+
+        // Determine the encryption mode
+        string mode = args[0];
+        if (!mode.StartsWith("-"))
+        {
+            Console.WriteLine("Invalid encryption mode.");
+            return;
+        }
+        mode = mode.Substring(1);
+
+        // Get the keyword and input/output file paths
+        string keyword = args[1];
+        string inputFile = args[2];
+        string outputFile = args.Length > 3 ? args[3] : inputFile + ".enc";
+
+        // Implement the encryption logic
+        switch (mode.ToLower())
+        {
+            case "rc4":
+                byte[] plainBytes = File.ReadAllBytes(inputFile);
+                byte[] keyBytes = Encoding.UTF8.GetBytes(keyword);
+                byte[] encryptedBytes = EncryptRC4(plainBytes, keyBytes);
+                File.WriteAllBytes(outputFile, encryptedBytes);
+                break;
+            case "xor":
+                // XOR encryption logic
+                break;
+            case "aes":
+                // AES encryption logic
+                break;
+            case "des":
+                // DES encryption logic
+                break;
+            default:
+                Console.WriteLine("Invalid encryption mode.");
+                break;
+        }
+    }
+
+    static byte[] EncryptRC4(byte[] data, byte[] key)
+    {
+        var rc4 = new RC4Engine();
+        var keyParam = new KeyParameter(key);
+        var cipher = new BufferedBlockCipher((IBlockCipher)rc4);
+        cipher.Init(true, keyParam);
+        return cipher.DoFinal(data);
+    }
+}
