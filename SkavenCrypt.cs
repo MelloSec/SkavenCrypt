30f8b037439cf1ba10f4652724bc3307e606a3a4
mellonaut <65114647+mellonaut@users.noreply.github.com>
2023-01-25 14:13:04 -0500

    Not working, but looks like it should. Dig in and debug see whats going on, it returned a 0 byte .enc file

--- SkavenCrypt.cs
+++ SkavenCrypt.cs
@@ -1,65 +1,93 @@
-﻿using Org.BouncyCastle.Crypto;
-using Org.BouncyCastle.Crypto.Engines;
-using Org.BouncyCastle.Crypto.Paddings;
+﻿using Org.BouncyCastle.Crypto.Engines;
 using Org.BouncyCastle.Crypto.Parameters;
-using Org.BouncyCastle.Security;
+using System;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
-class SkavenCrypt
+namespace SkavenCrypt
 {
-    static void Main(string[] args)
+    class SkavenCrypt
     {
-        // Check if the correct number of arguments have been provided
-        if (args.Length < 3)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Invalid number of arguments.");
-            return;
-        }
+            // Check if the correct number of arguments have been provided
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Invalid number of arguments.");
+                return;
+            }
 
-        // Determine the encryption mode
-        string mode = args[0];
-        if (!mode.StartsWith("-"))
-        {
-            Console.WriteLine("Invalid encryption mode.");
-            return;
-        }
-        mode = mode.Substring(1);
-
-        // Get the keyword and input/output file paths
-        string keyword = args[1];
-        string inputFile = args[2];
-        string outputFile = args.Length > 3 ? args[3] : inputFile + ".enc";
-
-        // Implement the encryption logic
-        switch (mode.ToLower())
-        {
-            case "rc4":
-                byte[] plainBytes = File.ReadAllBytes(inputFile);
-                byte[] keyBytes = Encoding.UTF8.GetBytes(keyword);
-                byte[] encryptedBytes = EncryptRC4(plainBytes, keyBytes);
-                File.WriteAllBytes(outputFile, encryptedBytes);
-                break;
-            case "xor":
-                // XOR encryption logic
-                break;
-            case "aes":
-                // AES encryption logic
-                break;
-            case "des":
-                // DES encryption logic
-                break;
-            default:
+            // Determine the encryption mode
+            string mode = args[0];
+            if (!mode.StartsWith("-"))
+            {
                 Console.WriteLine("Invalid encryption mode.");
-                break;
-        }
-    }
+                return;
+            }
+            mode = mode.Substring(1);
 
-    static byte[] EncryptRC4(byte[] data, byte[] key)
-    {
-        var rc4 = new RC4Engine();
-        var keyParam = new KeyParameter(key);
-        var cipher = new BufferedBlockCipher((IBlockCipher)rc4);
-        cipher.Init(true, keyParam);
-        return cipher.DoFinal(data);
+            // Get the keyword and input/output file paths
+            string keyword = args[1];
+            string inputFile = args[2];
+            string outputFile = args.Length > 3 ? args[3] : inputFile + ".enc";
+
+            // Implement the encryption logic
+            switch (mode.ToLower())
+            {
+                case "rc4":
+                    EncryptRC4(keyword, inputFile, outputFile);
+                    break;
+                case "xor":
+                    // XOR encryption logic
+                    break;
+                case "aes":
+                    // AES encryption logic
+                    break;
+                case "des":
+                    // DES encryption logic
+                    break;
+                default:
+                    Console.WriteLine("Invalid encryption mode.");
+                    break;
+            }
+        }
+
+        static async void EncryptRC4(string keyword, string inputFile, string outputFile)
+        {
+            // You encryption/decryption key as a bytes array
+            var key = Encoding.UTF8.GetBytes(keyword);
+            var cipher = new RC4Engine();
+            var keyParam = new KeyParameter(key);
+            // for decrypting the file just switch the first param here to false
+            cipher.Init(true, keyParam);
+
+            using (var inputStream = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
+            using (var outputStream = new FileStream(outputFile, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                // processing the file 4KB at a time.
+                byte[] buffer = new byte[1024 * 4];
+                long totalBytesRead = 0;
+                long totalBytesToRead = inputStream.Length;
+                while (totalBytesToRead > 0)
+                {
+                    // make sure that your method is marked as async
+                    int read = await inputStream.ReadAsync(buffer, 0, buffer.Length);
+
+                    // break the loop if we didn't read anything (EOF)
+                    if (read == 0)
+                    {
+                        break;
+                    }
+
+                    totalBytesRead += read;
+                    totalBytesToRead -= read;
+
+                    byte[] outBuffer = new byte[1024 * 4];
+                    cipher.ProcessBytes(buffer, 0, read, outBuffer, 0);
+                    await outputStream.WriteAsync(outBuffer, 0, read);
+                }
+            }
+        }
     }
 }
